a603989e5236f2df419eb6fc7d828c490ec601f5
daniel <daniel_amar@berkeley.edu>
2018-06-03 01:47:43 -0700

    add computer vision features

--- stopSign.py
+++ stopSign.py
@@ -14,32 +14,29 @@ from scipy.ndimage.filters import gaussian_filter
 from sklearn.decomposition import PCA
 from sklearn.neighbors import KNeighborsRegressor
 
+from skimage.feature import match_template
+
 
 # Tool functions
 def compute_red_percentage(img, treshold):
     return len(img[img>=treshold])/(img.shape[0]*img.shape[1])
-
-
-# Performance Evaluation of the model    
-def scoring(y_true, y_pred):
-    RMSE = np.sqrt(((y_true - y_pred)**2).mean())
-    MSE = ((y_true - y_pred)**2).mean()
-    print("\t Mean Squared Error: {0}".format(MSE))
-    print("\t RMSE: {0}".format(RMSE))
-    # SCORE - R^2
-    R2 = 1 - ((y_true - y_pred)**2).sum()/\
-          ((y_true - y_true.mean()) ** 2).sum()
-    print("\t R^2: {0}".format(R2))
+   
+def vectorize_single(image, keep_edge):
+    """ Convert the images into feature vectors of size (width x height)"""
+    temp_gray_img = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    temp_gray_img = gaussian_filter(temp_gray_img, sigma = 2)
     
+    if keep_edge: # we conserve only the edges in the image
+        temp_gray_img = cv2.Canny(np.uint8(temp_gray_img*255),30,140) 
+    
+    temp_gray_img = temp_gray_img.ravel()
+    return temp_gray_img
 
-def vectorize(images):
+def vectorize(images, keep_edge):
     """ Convert the images into feature vectors of size (width x height)"""
     X_vec = np.zeros((images.shape[0], images.shape[1]*images.shape[2]))
     for i in range(images.shape[0]):
-        temp_gray_img = cv2.cvtColor(images[i], cv2.COLOR_RGB2GRAY)
-        temp_gray_img = gaussian_filter(temp_gray_img, sigma = 2)
-        temp_gray_img = temp_gray_img.ravel()
-        X_vec[i] = temp_gray_img
+        X_vec[i] = vectorize_single(images[i], keep_edge)
     return X_vec
 
 
@@ -51,28 +48,52 @@ class StopSigns:
     max_heading = 80
     num_image_per_heading = 30
     
+    # Ratio of the training, validation and testing set
+    p_train = 0.6
+    p_val = 0.2
+    p_test= 0.2
+    
     # Parameters to tune
-    red_threshold = 200
+    #red_threshold = 200
+
     
     
     def __init__(self):
         """
-        Attributes
+        Attributes:
+            headings: all the possible range
+            
+            train_images, val_images, test_images: images in a (height, width, channel shapes)
+                regrouped in a 4-d array (used for the computer vision features detection)
+            train_labels, val_labels, test_labels: their corresponding headings
+            
+            X_train, X_val, X_test: the vectorized images (one row correspond to one image)
+            X_train_proj, X_val_proj, X_test_proj: their projection on the first principal vectors (PCA)
+            y_train, y_val, y_test: their corresponding labels (same as train_labels, ...)
+            
+                
+            
         """
         self.headings = np.array(range(self.min_heading,self.max_heading+1))
-        self.train_images, self.train_labels = 0, 0
-        self.val_images, self.val_labels = 0, 0
-        self.test_images, self.test_labels = 0, 0
+        self.train_images, self.train_labels = None, None
+        self.val_images, self.val_labels = None, None
+        self.test_images, self.test_labels = None, None
+        self.X_train, self.X_train_proj, self.y_train = None, None, None
+        self.X_val, self.X_val_proj, self.y_val = None, None, None
+        self.X_test, self.X_test_proj, self.y_test = None, None, None
+        self.y_train_pred, self.y_val_pred, self.y_test_pred = None, None, None
+        
         
         """
         Fitting parameters
         """
-        w_red_percentage = 0
-        
+        #self.w_red_percentage = [] NOT USED
+        self.pca = None
+        self.neigh = None
         
         
     
-    def split_data(self, p_train = 0.6, p_val = 0.2, p_test =  0.2):
+    def split_data(self, p_train = p_train, p_val = p_val, p_test =  p_test):
         """
         Split the data into a training, validation and testing set with the 
         corresonding percentages
@@ -123,10 +144,109 @@ class StopSigns:
         print("Splitting done!")
     
 
-
-
-
-    def preprocessing(self, images):
+    def vectorize_sets(self, keep_edge = 0):
+        """
+        This function is preprocessing the data and store them in a matrix where
+        each row represents a 275x275 image.
+        The preprocessing step includes:
+            covert the image in grayscale
+            keep the edge of the image only (keep_edge parameter on/off)
+            
+        """
+        print("Vectorizing data")
+        self.X_train = vectorize(self.train_images, keep_edge)
+        self.X_val = vectorize(self.val_images, keep_edge)
+        self.X_test = vectorize(self.test_images, keep_edge)
+        self.y_train = self.train_labels
+        self.y_val = self.val_labels
+        self.y_test = self.test_labels
+        
+        
+        
+        
+    def do_pca(self, k ,plot = 0):
+        print("Principal Component Analysis")
+        self.pca = PCA(n_components=k)
+        self.pca.fit(self.X_train)
+        print("\t explained variance ratio_: ",self.pca.explained_variance_ratio_) 
+        print("\t total explained variance ratio",self.pca.explained_variance_ratio_.sum())
+        if plot:
+            plt.title("explained variance ratio function of the i_th component")
+            plt.plot(self.pca.explained_variance_ratio_)
+            plt.show()
+            
+            
+    def pca_transform(self, plot = 0):
+        print("Computing the Principal Components")
+        self.X_train_proj = self.pca.transform(self.X_train)
+        self.X_val_proj = self.pca.transform(self.X_val)
+        self.X_test_proj = self.pca.transform(self.X_test)
+        
+        if plot:
+            num_training = int(self.num_image_per_heading*self.p_train)
+            n = self.max_heading-self.min_heading+1
+            for i in range(n):
+                plt.scatter(self.X_train_proj[:,0][num_training*i:num_training*(i+1)]\
+                            , self.X_train_proj[:,1][num_training*i:num_training*(i+1)]\
+                            , c=str(i/n))
+            plt.title("2D projection of the images after PCA (grayscale represents the heading)")
+            plt.show()
+            
+            
+    def fit(self, n_neighbors, verbose = 1):
+        if verbose:
+            print("Fitting the training data with k-nearest neighbors")
+        self.neigh = KNeighborsRegressor(n_neighbors=n_neighbors)
+        self.neigh.fit(self.X_train_proj, self.y_train) 
+        
+        
+    def predict(self):
+        self.y_train_pred = self.neigh.predict(self.X_train_proj).astype(np.int8)
+        self.y_val_pred = self.neigh.predict(self.X_val_proj).astype(np.int8)
+        self.y_test_pred = self.neigh.predict(self.X_test_proj).astype(np.int8)
+        
+      
+    def score(self, mode):
+        if mode == "training":
+            y_true = self.y_train
+            y_pred = self.y_train_pred
+        elif mode == "validation":
+            y_true = self.y_val
+            y_pred = self.y_val_pred
+        elif mode == "test":
+            y_true = self.y_test
+            y_pred = self.y_test_pred
+        else: 
+            raise NameError("invalid mode parameter. training, validation or test accepted")
+        print ("Evaluation on the {} set:".format(mode))
+        RMSE = np.sqrt(((y_true - y_pred)**2).mean())
+        MSE = ((y_true - y_pred)**2).mean()
+        print("\t Mean Squared Error: {0}".format(MSE))
+        print("\t RMSE: {0}".format(RMSE))
+        # SCORE - R^2
+        R2 = 1 - ((y_true - y_pred)**2).sum()/\
+              ((y_true - y_true.mean()) ** 2).sum()
+        print("\t R^2: {0}".format(R2)) 
+            
+        
+    def accuracy(self, mode):
+        if mode == "training":
+            y_true = self.y_train
+            y_pred = self.y_train_pred
+        elif mode == "validation":
+            y_true = self.y_val
+            y_pred = self.y_val_pred
+        elif mode == "test":
+            y_true = self.y_test
+            y_pred = self.y_test_pred
+        else: 
+            raise NameError("invalid mode parameter. training, validation or test accepted")
+        classif = y_true - y_pred
+        acc = classif[classif==0].shape[0]/y_true.shape[0]
+        print ("Accuracy of the {0} set: {1}".format(mode, acc))
+        return classif[classif==0].shape[0]
+        
+    def red_preprocessing(self, images):
         """
         Images are drawn from the training, validation or testing set.
             images is usually: self.train_images, self.val_images, or self.test_images
@@ -141,14 +261,13 @@ class StopSigns:
             red_percentages_.append(red)
         X = np.array(red_percentages_).reshape((-1,1))
         
-        
         return X
             
     
 
 
     
-    def fit(self, X, y, plot = 0):        
+    def fit_red(self, X, y, plot = 0):        
         
         # compute the absolute value based on the red percentage on the image
         H = np.hstack([np.ones((y.shape[0],1)),\
@@ -170,38 +289,118 @@ class StopSigns:
             plt.scatter(y[:120], X[:120,0], s = 1, c ='r')
             plt.show() 
             
-            
-            
-    
-            
-        
-        
+
         
 
 if __name__ == "__main__":
     
+    # Loading, splitting and preprocessing the data
+    model = StopSigns()
+    model.split_data()
+    model.vectorize_sets(keep_edge=1)
+    
+    # PCA Looking at the explained variance by the principal components and choosing k
+    model.do_pca(10, plot = 1)
+
+    nb_principal_components = 5
+    model.do_pca(nb_principal_components)
+    
+    # Project the data + 2D visualization
+    model.pca_transform(plot = 1)
+    
+    n_neighbors = 3
+    model.fit(n_neighbors)
+    
+    model.predict()
+
+    model.score("training")
+    model.score("validation")
+    model.score("test")
+    
+    model.accuracy("training")
+    model.accuracy("validation")
+    model.accuracy("test")
+
+
+
+
+
+    """ For a new image """
+    
+    res=[]
+    real_img = mpimg.imread("real_images/1.jpg")
+    real_img_vec = vectorize_single(real_img, keep_edge=1)
+    real_img_proj = model.pca.transform(real_img_vec.reshape((1,-1)))
+    for n_neighbors in range(1,50):
+        model.fit(n_neighbors, verbose=0)
+        res.append(model.neigh.predict(real_img_proj)[0][0])
+    plt.title("Heading function of the number of nearest neighbors")
+    plt.plot(res)
+    plt.show()
+    
+    
+    
+    
+    
+    
+    """ Computer Vision Features """
+    """
+    
+    # RED PERCENTAGE ATTEMPT
+    red_threshold = 200
+    
+    real_image_red_percentage = compute_red_percentage(real_img, red_threshold)
+        
+    red_distribution = []
+    for i in range(model.train_images.shape[0]):
+        red = compute_red_percentage(model.train_images[i],red_threshold)
+        red_distribution.append(red)
+    red_distribution = np.array(red_distribution).reshape((-1,1))
+    
+    plt.plot(model.headings,red_distribution[::18],c='r')
+    plt.plot(model.headings,np.full((161,1),real_image_red_percentage),c='b')
+    plt.show()
+    
+    
+    # TEMPLATE MATCHING ATTEMPT
+    NW = mpimg.imread("templates/NW.jpg")
+    NW = gaussian_filter(NW, sigma = 2)
+    im1 = model.train_images[81*18]
+    im2 = model.train_images[2*18]
 
     
-    split_done = 0
-    if not split_done:
-        model = StopSigns()
-        model.split_data()
-        
-    preprocessed = 0
-    if not preprocessed:
-        X_train = model.preprocessing(model.train_images)
-        y_train = model.train_labels
-        
-        y_test = model.test_labels
-        y_val = model.val_labels
-        
-    model.fit(X_train,y_train, plot=1)
+
+
+    plt.title("template North-West")
+    plt.imshow(NW)
+    plt.show()
+    correlation_map = match_template(im2, NW)
+    plt.imshow(correlation_map[:,:,0], cmap="gray")
+    plt.show()
+    print("The template position is at: ",np.unravel_index(correlation_map.argmax()\
+                            , correlation_map.shape))
     
     
-   
-        
-    
-        
+    # EDGES
+
+
+    im3 = real_img.copy()
+    im3 = gaussian_filter(im3, sigma = 2)
+    im3=im2.copy()
+    im3 = gaussian_filter(im3, sigma = 1.5)
+    plt.imshow(im3)
+    plt.show()
+    print("############# Canny Edge detector ############# ")
+    grayImg = cv2.cvtColor(im3, cv2.COLOR_RGB2GRAY) 
+    plt.imshow(grayImg,cmap="gray")
+    plt.show()
+    cannyEdges = cv2.Canny(np.uint8(grayImg*255),30,140) #accept only uint8 images
+    plt.title("Canny Edge detector")
+    plt.imshow(cannyEdges,cmap="gray")
+    plt.show()
+    """
+
+            
     """ SANDBOX """
     """
     # original image
@@ -238,126 +437,6 @@ if __name__ == "__main__":
     
     
     
-    """
-    # Machine Learning approach
-    # VECTORIZATION + PCA + K-NN
-
-        
-    X_train = vectorize(model.train_images)
-        
-    """ PCA and 2-D visualization """
-    
-    # How many components ?
-    pca = PCA(n_components=10)
-    pca.fit(X_train)
-    print("explained_variance_ratio_: ",pca.explained_variance_ratio_)  
-    plt.title("explained variance ratio function of the i_th component")
-    plt.plot(pca.explained_variance_ratio_)
-    plt.show()
-    
-    nb_components = 5
-    pca = PCA(n_components=nb_components)
-    pca.fit(X_train)
-    X_train_proj = pca.transform(X_train)
-    print("explained_variance_ratio_: ",pca.explained_variance_ratio_)   
-    for i in range(161):
-        plt.scatter(X_train_proj[:,0][18*i:18*(i+1)], X_train_proj[:,1][18*i:18*(i+1)], c=str(i/161.))
-    plt.title("2D projection of the images after PCA")
-    plt.show()
-    
-    
-    
-    
-    """ K-NN on 5 principal components """
-    
-    neigh = KNeighborsRegressor(n_neighbors=3)
-    neigh.fit(X_train_proj, y_train) 
-    
-    X_test = vectorize(model.test_images)
-    X_test_proj = pca.transform(X_test)
-    y_test_predict = neigh.predict(X_test_proj)
-    y_test_predict = y_test_predict.astype(np.int8)
-    
-    
-    # SCORE 
-        
-    print("Evaluation on the Test Set")
-    scoring(y_test, y_test_predict)
-        
-
-    
-    """ RESULTS on Test Set
-    Mean Squared Error: 0.018633540372670808
-    RMSE: 0.136504726557987
-    R^2 (explained variance): 0.9999913733609386
-    """
-    
-    """ on the validation set """ 
-    X_val = vectorize(model.val_images)
-    X_val_proj = pca.transform(X_val)
-    y_val_predict = neigh.predict(X_val_proj)
-    y_val_predict = y_val_predict.astype(np.int8)
-    
-    print("Evaluation on the Validation Set")
-    scoring(y_val, y_val_predict)
-        
-    """ RESULTS on Validation Set
-    Mean Squared Error: 0.013457556935817806
-    RMSE: 0.11600671073613718
-    R^2 (explained variance): 0.9999937696495668
-
-    """
-    
-    
-    
-    
-    
-    # Machine Learning + Computer Vision Approach
-    # 5-PCA + Red Percentage + other CV features
-    X_train_proj.shape
-    
-    red_percentages_ = []
-    red_threshold = 200
-    for i in range(model.train_images.shape[0]):
-        red = compute_red_percentage(model.train_images[i],red_threshold)
-        red_percentages_.append(red)
-    red_percentages_ = np.array(red_percentages_).reshape((-1,1))
-    X_train_proj = np.hstack([X_train_proj, red_percentages_])
-    
-    
-    from sklearn.preprocessing import normalize
-    X_train_proj = normalize(X_train_proj)
-    
-    
-
-
-    """
-    Full K-NN
-    
-
-    neigh = KNeighborsRegressor(n_neighbors=3)
-    neigh.fit(X_train, y_train) 
-    
-    X_test = vectorize(model.test_images)
-    y_test_predict = neigh.predict(X_test)
-    y_test_predict = y_test_predict.astype(np.int8)
-    
-    # SCORE - RMSE
-    RMSE = np.sqrt(((y_test - y_test_predict)**2).mean())
-    MSE = ((y_test - y_test_predict)**2).mean()
-    print("Mean Squared Error: {0}".format(MSE))
-    print("RMSE: {0}".format(RMSE))
-    # SCORE - R^2
-    R2 = 1 - ((y_test - y_test_predict)**2).sum()/\
-          ((y_test - y_test.mean()) ** 2).sum()
-    print("R^2 (explained variance): {0}".format(R2))
-    
-
-    
-    # RESULTS on Test Set
-    Mean Squared Error: 0.031055900621118012
-    RMSE: 0.17622684421256035
-    R^2 (explained variance): 0.999985622268231
     """
 
 
